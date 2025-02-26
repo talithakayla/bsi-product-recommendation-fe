e47c8690f4c56ff04897bfe42c67f0597dc4ed50
Damar Buana Murti <damarbuanamurti@gmail.com>
2025-02-26 19:10:51 +0700

    feat: restyle CTA button

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -46,15 +46,17 @@
         </li>
       </ul>
     </div>
-    <div>
+    <button
+      class="flex flex-row justify-between bg-[#F7AF2E] text-white px-[30px] py-3 rounded-[10px] cursor-pointer hover:brightness-[85%] transition-all my-[50px] font-semibold ml-auto"
+    >
       <a
         href="https://play.google.com/store/apps/details?id=co.id.bankbsi.superapp&hl=id"
         target="_blank"
-        class="flex flex-row justify-between bg-[#F7AF2E] text-white px-[30px] py-3 rounded-[10px] ml-auto cursor-pointer hover:brightness-[85%] transition-all my-[50px] font-semibold"
+        class=""
       >
         Ajukan Sekarang <span class="ml-[84px]">&rarr;</span>
       </a>
-    </div>
+    </button>
   </section>
 </template>
 

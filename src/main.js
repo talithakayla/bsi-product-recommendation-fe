5f1304e3897e03546a020ab40b55d4b54665577f
Damar Buana Murti <damarbuanamurti@gmail.com>
2025-02-26 10:36:01 +0700

    feat: implement vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-// import './assets/main.css';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
 
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(router);
+
+app.mount("#app");
